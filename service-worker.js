2a27696956ef89f71eccc9658eb902d48a1bc23f
mallchel <sebastianalfabc@gmail.com>
2020-10-23 21:08:49 +0300

    update

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d8c1a1a7ed55c112f914f8f936d051d4.js"
+  "/precache-manifest.323af63b386d2277e6e964d43d068415.js"
 );
 
 self.addEventListener('message', (event) => {
